76385860fc50bbd8a0c522bac1065e4979a7eb0c
Camilleri <ssancam391@g.educaand.es>
2024-10-01 18:48:45 +0200

    ejercicio4

--- Ejercicio_clase_JavaScript_HelloWorld/Hoja_Ejercicios_JavaScript_fundamentals/ejercicio03.js
+++ Ejercicio_clase_JavaScript_HelloWorld/Hoja_Ejercicios_JavaScript_fundamentals/ejercicio03.js
@@ -4,15 +4,15 @@ Ejercicio 3: Operadores y Asignaciones
 2. Utiliza un operador de incremento y uno de decremento en las variables, mostrando el resultado en consola.
 */
 
-let num1 = 5
-let num2 = 3
+let num1 = 5 ;
+let num2 = 3 ;
 
-console.log(num1 + num2)
-console.log(num1 - num2)
-console.log(num1 * num2)
-console.log(num1 / num2)
+console.log(num1 + num2) ;
+console.log(num1 - num2) ;
+console.log(num1 * num2) ;
+console.log(num1 / num2) ;
 
-num1++
-num2--
-console.log(num1)
-console.log(num2)
+num1++ ;
+num2-- ;
+console.log(num1) ;
+console.log(num2) ;
