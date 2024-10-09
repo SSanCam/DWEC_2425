b2179ed098c6654366fcfd253c51f521ed4f039c
Camilleri <ssancam391@g.educaand.es>
2024-10-09 17:21:38 +0200

    e5

---
+++ Entrega_Actividad_A02/e4-SanchezCamilleri-Sara.js
@@ -0,0 +1,15 @@
+/*
+Ejercicio4:MatricesyAplanado
+Dadaunamatrizbidimensionaldeenteros,creaunafunciónque:
+- AplanelamatrizsinutilizarArray.flat().
+- Calculeelpromediodeloselementosdelamatriz.
+*/
+
+let matriz = [
+    ["1","2","3"],  
+    ["4","5","6"],  
+    ["7","8","9"]
+        ];
+
+alert(matriz.flat());
+
