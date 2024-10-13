4e277cebc013780cfd67bc9342a77a772b2cc6a7
Camilleri <ssancam391@g.educaand.es>
2024-10-13 09:57:38 +0200

    e7

---
+++ Entrega_Actividad_A02/e7-SanchezCamilleri-Sara.js
@@ -0,0 +1,45 @@
+/*
+Ejercicio7:JerarquíadeClases
+Define una clase Animal con propiedadescomonombreyedad,yunmétodohablar().Creados
+subclases:PerroyGato,queheredendeAnimalysobreescribanelmétodohablar().
+*/
+class Animal {
+    nombre;
+    edad;
+
+    constructor(nombre, edad) {
+        this.nombre = nombre,
+        this.edad = edad
+    }
+
+    hablar (){
+        alert("Éste animal está hablando!");
+    }
+}
+/*
+let perro = new Animal("El perro bob", 10);
+let gato = new Animal("El gato calcetines", 5);
+gato.hablar("asd");
+*/
+class Perro extends Animal{
+    constructor(nombre, edad){
+        super(nombre, edad);
+    }
+    hablar (){
+        alert("Éste animal hace guau!!");
+    } 
+}
+
+class Gato extends Animal {
+    constructor(nombre, edad){
+        super(nombre, edad);
+    }
+    hablar () {
+        alert("Éste animal hace miau!!!!");
+    }
+}
+
+let perro = new Perro("El perro bob", 5);
+perro.hablar;
+let gato = new Gato("Calcetines");
+gato.hablar;
